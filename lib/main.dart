4433ef05c5d938401f6a707af4356564f644fa8c
djeisonCassimiro <djeison.dev@gmail.com>
2021-04-09 20:55:44 -0300

    ajuste de imports e dependencias

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'file:///D:/Projetos/Curso%20Flutter/Carro/carros/lib/pages/login/login_page.dart';
 import 'package:flutter/material.dart';
+import 'pages/login/login_page.dart';
 
 void main() {
   runApp(MyApp());
